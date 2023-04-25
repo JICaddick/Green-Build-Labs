5d1d10dadbbdccb62b53cffed58ceea4db87f9c9
JICaddick <james_caddick16@hotmail.com>
2023-04-25 13:13:08 -0500

    create.table.sql up-to-date as of the 25th of April. Trigger for 'total' still not working. Concat for carbon values not working. Those and final calculation pending.

--- create_table.sql
+++ create_table.sql
@@ -1,21 +1,29 @@
 /* Users table*/
-CREATE TABLE green_build_labs.`user` (
-	id INT auto_increment NOT NULL,
-	email varchar(100) NOT NULL,
-	password varchar(100) NOT NULL,
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP NULL,
-	role ENUM('individual', 'contractor') NOT NULL,
-	CONSTRAINT user_PK PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `role` enum('individual','contractor','contractor_team') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) 
+ENGINE=InnoDB AUTO_INCREMENT=37 
+DEFAULT CHARSET=utf8mb4 
 COLLATE=utf8mb4_general_ci;
 
 /*system_permissions*/
-CREATE TABLE `system_permissions` (
+REATE TABLE `system_permissions` (
   `project_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `permissions` enum('read','read_write') NOT NULL DEFAULT 'read',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `has_read_access` tinyint(1) DEFAULT 1,
+  `can_create_contractor_team_users` tinyint(1) DEFAULT 1,
+  `can_delete_contractor_team_user` tinyint(1) DEFAULT 1,
+  `can_create_project` tinyint(1) DEFAULT 1,
+  `can_edit_project` tinyint(1) DEFAULT 1,
+  `can_delete_project` tinyint(1) DEFAULT 1,
+  `can_create_multiple_projects` tinyint(1) DEFAULT 1,
+  PRIMARY KEY (`id`),
   KEY `project_team_members_FK` (`project_id`),
   KEY `project_team_members_FK_1` (`user_id`),
   CONSTRAINT `project_team_members_FK` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
@@ -26,12 +34,16 @@ DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
 
 /* project*/
-CREATE TABLE green_build_labs.`project` (
-	id INT auto_increment NOT NULL,
-	name varchar(100) NOT NULL,
-	start_date DATE NOT NULL,
-	end_date DATE NOT NULL,
-	CONSTRAINT Build_Project_PK PRIMARY KEY (id)
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `project_FK` (`user_id`),
+  CONSTRAINT `project_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `end_date_later_than_start` CHECK (`end_date` > `start_date`)
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
@@ -42,8 +54,9 @@ CREATE TABLE `materials` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` enum('timber','cement','glass') NOT NULL,
   `unit` enum('kgs','l') NOT NULL,
-  `carbon_emissions_per_unit` varchar(50) NOT NULL DEFAULT 'CO2e',
-  PRIMARY KEY (`id`)
+  `carbon_emissions_per_unit` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_material_name` (`name`)
 ) 
 ENGINE=InnoDB 
 DEFAULT CHARSET=utf8mb4 
@@ -54,7 +67,12 @@ CREATE TABLE `project_materials` (
   `project_id` int(11) NOT NULL,
   `material_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `total` varchar(50) NOT NULL,
+  `total` int(11) DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` enum('timber','cement','glass') DEFAULT NULL,
+  `unit` enum('kgs','l') DEFAULT NULL,
+  `carbon_emissions_per_unit` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `project_materials_FK` (`project_id`),
   KEY `project_materials_FK_1` (`material_id`),
   CONSTRAINT `project_materials_FK` FOREIGN KEY (`project_id`) REFERENCES `project` (`id`),
