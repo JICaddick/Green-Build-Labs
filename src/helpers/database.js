0516de13b7a9afbe9e9465b6009a7b12decfb950
JICaddick <james_caddick16@hotmail.com>
2023-05-23 17:14:32 -0500

    db file switched back to js

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,4 +1,4 @@
-import mariadb from "mariadb";
+const mariadb = require('mariadb');
 require('dotenv').config();
 // a pool is a bunch of connections or something that defines a connection to the database. It's a pool of connections.
 // connectionlimit of 5 means no more than 5 symultaneous connections to the database at any given time.
@@ -8,12 +8,11 @@ const pool = mariadb.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  connectionLimit: 5 
+  connectionLimit: 5
 });
 
-pool.getConnection().then((connection) => {
-  if (connection) connection.release();
-}) .catch((err) => {
+// Connect and check for errors
+pool.getConnection((err, connection) => {
   if(err){
     if (err.code === 'PROTOCOL_CONNECTION_LOST') {
       console.error('Database connection was closed');
@@ -25,6 +24,8 @@ pool.getConnection().then((connection) => {
       console.error('Database connection was refused');
     }
   }
-})
+  if (connection) connection.release();
+  return;
+} );
 
-export { pool }
+module.exports = pool;
