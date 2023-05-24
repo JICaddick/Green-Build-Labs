bcc28a982b67cd4e28dfc11fc225986c08917262
JICaddick <james_caddick16@hotmail.com>
2023-05-23 21:32:14 -0500

    types added (pg) for permissions.ts - preemptive, I know but the lone js file in that folder was sad.

--- src/helpers/permissions.ts
+++ src/helpers/permissions.ts
@@ -1,17 +1,43 @@
-// // const pool = require('../helpers/database');
+// import { Pool } from 'pg';
+// import { QueryResult } from 'pg';
+
+// const pool = new Pool();
+
+// interface User {
+//   id: number;
+//   role: string;
+// }
+
+// interface Permissions {
+//   has_read_access: boolean;
+//   can_create_project: boolean;
+//   can_edit_project: boolean;
+//   can_delete_project: boolean;
+//   can_create_multiple_projects: boolean;
+//   can_create_contractor_team_users: boolean;
+//   can_delete_ct_user: boolean;
+// }
 
 // async function setPermissions() {
 //   try {
-//     //sqlGetUsers is a query that gets all users from the users table
+//     // sqlGetUsers is a query that gets all users from the users table
 //     const sqlGetUsers = 'SELECT id, role FROM users';
 //     // the users variable is an array of objects. Each object is a user.
-//     const users = await pool.query(sqlGetUsers);
+//     const { rows: users }: QueryResult<User> = await pool.query(sqlGetUsers);
 
 //     for (const user of users) {
 //       const userId = user.id;
 //       const roleName = user.role;
-      
-//       let permissions = {};
+
+//       let permissions: Permissions = {
+//         has_read_access: false,
+//         can_create_project: false,
+//         can_edit_project: false,
+//         can_delete_project: false,
+//         can_create_multiple_projects: false,
+//         can_create_contractor_team_users: false,
+//         can_delete_ct_user: false
+//       };
 
 //       switch (roleName) {
 //         case 'individual':
@@ -52,17 +78,17 @@
 //       }
 
 //       const sqlSetPermissions = `
-//       UPDATE system_permissions 
-//       SET 
-//         has_read_access = ${permissions.has_read_access || false},
-//         can_create_contractor_team_users = ${permissions.can_create_contractor_team_users || false},
-//         can_create_project = ${permissions.can_create_project || false},
-//         can_edit_project = ${permissions.can_edit_project || false},
-//         can_delete_project = ${permissions.can_delete_project || false},
-//         can_delete_ct_user = ${permissions.can_delete_ct_user || false},
-//         can_create_multiple_projects = ${permissions.can_create_multiple_projects || false}
-//       WHERE user_id = ${userId}
-//     `;
+//         UPDATE system_permissions 
+//         SET 
+//           has_read_access = ${permissions.has_read_access || false},
+//           can_create_contractor_team_users = ${permissions.can_create_contractor_team_users || false},
+//           can_create_project = ${permissions.can_create_project || false},
+//           can_edit_project = ${permissions.can_edit_project || false},
+//           can_delete_project = ${permissions.can_delete_project || false},
+//           can_delete_ct_user = ${permissions.can_delete_ct_user || false},
+//           can_create_multiple_projects = ${permissions.can_create_multiple_projects || false}
+//         WHERE user_id = ${userId}
+//       `;
 //       await pool.query(sqlSetPermissions);
 //     }
 //     console.log('Permissions have been set for all roles');
@@ -71,4 +97,4 @@
 //   }
 // }
 
-// // module.exports = { setPermissions };
+// export { setPermissions };
