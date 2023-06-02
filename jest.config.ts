3fd2db0286a35e276780b5a80644b9c2843a9920
JICaddick <james_caddick16@hotmail.com>
2023-06-02 13:49:50 -0500

    config corrected. test test passing.

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,27 @@
-import type {Config} from '@jest/types';
+import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+  // Set the test environment
+  testEnvironment: 'node',
+
+  // Set the file patterns for tests
+  testMatch: [
+    '**/tests/**/*.test.ts'
+  ],
+
+  // Set the verbose option
   verbose: true,
+
+  // Set the transform configuration
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+
+  // Optional: Set up any other Jest configurations as needed
+  // ...
+
+  // Optional: Set up any Jest coverage configurations as needed
+  // ...
 };
 
 export default config;
