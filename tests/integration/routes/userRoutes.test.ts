2d6239c33b19ea0980039aaa47e8b11ca7f4ca5c
JICaddick <james_caddick16@hotmail.com>
2023-06-03 21:10:08 -0500

    2 more user tests passing.

--- tests/integration/routes/userRoutes.test.ts
+++ tests/integration/routes/userRoutes.test.ts
@@ -2,36 +2,60 @@ import request from 'supertest';
 import app from '../../../src/app';
 
 describe('User Routes', () => {
-  it('should respond with a list of users', async () => {
+  let user;
+
+  beforeAll(async () => {
     const response = await request(app).get('/user/getallusers');
-
-    expect(response.status).toBe(200);
-    expect(response.body).toBeInstanceOf(Array); // Ensure the response is an array
-
-    // Assuming you have at least one user in the response
-    const user: {
-      id: number;
-      email: string;
-      password: string;
-      created_at: Date;
-      role: 'individual' | 'contractor' | 'contractor_team';
-    } = response.body[0];
-
-    expect(user).toHaveProperty('id');
-    expect(user).toHaveProperty('email');
-    expect(user).toHaveProperty('password');
-    expect(user).toHaveProperty('created_at');
-    expect(user).toHaveProperty('role');
-
-    // You can further validate the values or types of the properties if needed
-    expect(typeof user.id).toBe('number');
-    expect(typeof user.email).toBe('string');
-    expect(typeof user.password).toBe('string');
-    // expect(user.created_at).toBeInstanceOf(Date);
-    expect(['individual', 'contractor', 'contractor_team']).toContain(user.role);
-
-    // Add more assertions...
+    user = response.body;
   });
 
-  // Add more route tests...
+  it('should respond with a list of users', async () => {
+    expect(user).toBeInstanceOf(Array);
+    expect(user.length).toBeGreaterThan(0);
+  
+    user.forEach((userObj) => {
+      expect(userObj).toHaveProperty('id');
+      expect(userObj).toHaveProperty('email');
+      expect(userObj).toHaveProperty('password');
+      expect(userObj).toHaveProperty('created_at');
+      expect(userObj).toHaveProperty('role');
+  
+      expect(typeof userObj.id).toBe('number');
+      expect(typeof userObj.email).toBe('string');
+      expect(typeof userObj.password).toBe('string');
+      // expect(userObj.created_at).toBeInstanceOf(Date);
+      expect(['individual', 'contractor', 'contractor_team']).toContain(userObj.role);
+    });
+  });
+  
+
+  it('should get a user by id', async () => {
+    const id = user[0].id
+    const response = await request(app).get(`/user/${id}/getuserbyid`);
+    expect(response.status).toBe(200);
+    // Add more assertions to validate the response body
+  });
+
+  it('should create a new user', async () => {
+    const response = await request(app).post('/user/register').send({
+      "email": "test@testtest.test",
+      "password": "potatoes",
+      "role": "contractor_team"
+    });
+    expect(response.status).toBe(200);
+    expect(response.body).toBe("New user created with email test@testtest.test");
+
+    const getAllUserResponse = await request(app).get('/user/getallusers');
+    user = getAllUserResponse.body;
+
+    expect(getAllUserResponse.status).toBe(200);
+    const registeredUser = user.find((user) =>  user.email === "test@testtest.test");
+
+    expect(registeredUser).toHaveProperty('id');
+    expect(registeredUser).toHaveProperty('email');
+    expect(registeredUser).toHaveProperty('password');
+    expect(registeredUser).toHaveProperty('created_at');
+    expect(registeredUser).toHaveProperty('role');
+    
+  });
 });
